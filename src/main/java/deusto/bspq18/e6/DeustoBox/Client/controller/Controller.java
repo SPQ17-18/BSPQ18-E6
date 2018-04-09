a75e5680b505357be6c9fd154cfd3aa567d31a76
Aitor Ugarte <aitorugarte@opendeusto.es>
2018-04-09 09:24:26 +0200

    hello Maven!

---
+++ src/main/java/deusto/bspq18/e6/DeustoBox/Client/controller/Controller.java
@@ -0,0 +1,5 @@
+package deusto.bspq18.e6.DeustoBox.Client.controller;
+
+public class Controller {
+
+}
---
+++ src/main/java/deusto/bspq18/e6/DeustoBox/Server/remote/ClientUtils.java
@@ -0,0 +1,5 @@
+package deusto.bspq18.e6.DeustoBox.Server.remote;
+
+public class ClientUtils {
+
+}
