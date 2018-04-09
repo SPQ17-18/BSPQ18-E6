cbc0636a33a856b4013ee27441fc106a21cc7b0b
Aitor Ugarte <aitorugarte@opendeusto.es>
2018-04-09 11:56:36 +0200

    idea cliente

--- src/main/java/deusto/bspq18/e6/DeustoBox/Client/gui/idea_client.java
+++ src/main/java/deusto/bspq18/e6/DeustoBox/Client/gui/idea_client.java
@@ -1,30 +1,34 @@
 package deusto.bspq18.e6.DeustoBox.Client.gui;
 
+import java.io.File;
 
 public class idea_client {
 	
+			// Cargar el típico instalador
+			/*
+			 * Pedir ruta donde estará la Box.
+			 * Progressbar mientras intala todo.
+			 * 
+			 */
+			
+			// Ejecutar el programa
+			/*
+			 * Login o Registro.
+			 * GUI que esté activa mientras se está ejecutando el programa.
+			 * Se sincroniza la Box.
+			 * 
+			 */
+			
+			// Sincronizar la Box
+			/*
+			 * Comprobar si se ha modificado algún archivo.
+			 * Descargar/Subir los archivos.
+			 * 
+			 */
 	public static void main(String[] args) {
+		String dir = "C:\\\\Users\\\\deusto_06\\\\Desktop";
+		File directorio=new File(dir + "\\Deusto-Box"); 
+		directorio.mkdir(); 
 		
-		// Cargar el típico instalador
-		/*
-		 * Pedir ruta donde estará la Box.
-		 * Progressbar mientras intala todo.
-		 * 
-		 */
-		
-		// Ejecutar el programa
-		/*
-		 * Login o Registro.
-		 * GUI que esté activa mientras se está ejecutando el programa.
-		 * Se sincroniza la Box.
-		 * 
-		 */
-		
-		// Sincronizar la Box
-		/*
-		 * Comprobar si se ha modificado algún archivo.
-		 * Descargar/Subir los archivos.
-		 * 
-		 */
 	}
 }
