19d6c2613f099ad7470199b133cb5515e606d84b
Aitor Ugarte <aitorugarte@opendeusto.es>
2018-04-13 20:03:19 +0200

    remote facade implemented!

--- src/main/java/deusto/bspq18/e6/DeustoBox/Server/remote/IClientUtils.java
+++ src/main/java/deusto/bspq18/e6/DeustoBox/Server/remote/IClientUtils.java
@@ -1,6 +1,13 @@
 package deusto.bspq18.e6.DeustoBox.Server.remote;
 
-public interface IClientUtils {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+import deusto.bspq18.e6.DeustoBox.Server.dto.UserDTO;
+
+public interface IClientUtils extends Remote{
+
+	public UserDTO login(String username, String password) throws RemoteException;
+	public void registerUser(UserDTO userdto) throws RemoteException;
 	
 }
