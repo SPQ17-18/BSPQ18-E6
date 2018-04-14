6471a6d6385cf1aaaad8ce11d153bd4bd5f509a5
Markel <Markel@Markel-PC.home>
2018-04-14 15:02:54 +0200

    Service Locator OK!

--- src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/remote/RMIServiceLocator.java
+++ src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/remote/RMIServiceLocator.java
@@ -1,5 +1,32 @@
 package es.deusto.bspq18.e6.DeustoBox.Client.remote;
 
+import es.deusto.bspq18.e6.DeustoBox.Server.remote.IDeustoBoxRemoteService;
+
 public class RMIServiceLocator {
+
+	private IDeustoBoxRemoteService façade;
+	
+public void setService(String [] args){
+
+	
+	
+String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
+
+System.out.println(name);
+try {
+	this.façade =(IDeustoBoxRemoteService) java.rmi.Naming.lookup(name);
+}
+catch (Exception ex) {
+	
+	System.out.println("An error has happened while setting the service");
+}
+	
+}
+
+
+public IDeustoBoxRemoteService getService(){
+	return this.façade;
+	
+}
 
 }
