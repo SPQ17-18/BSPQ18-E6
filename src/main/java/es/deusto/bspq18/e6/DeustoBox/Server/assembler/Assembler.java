add51404390d4d95d687fcec6fc58578a7ba5368
Markel <Markel@Markel-PC.home>
2018-04-15 18:21:58 +0200

    ads

---
+++ src/main/java/es/deusto/bspq18/e6/DeustoBox/Server/assembler/Assembler.java
@@ -0,0 +1,16 @@
+package es.deusto.bspq18.e6.DeustoBox.Server.assembler;
+
+import es.deusto.bspq18.e6.DeustoBox.Server.dto.UserDTO;
+import es.deusto.bspq18.e6.DeustoBox.Server.jdo.data.User;
+
+public class Assembler {
+
+	public UserDTO userDTO(User user) {
+		return new UserDTO(user.getEmail(), user.getName(), user.getPassword());
+	}
+	
+	public User user(UserDTO userdto) {
+		return new User(userdto.getEmail(), userdto.getName(), userdto.getPassword());
+	}
+	
+}
