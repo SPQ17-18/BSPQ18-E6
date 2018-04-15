9112b5cbc7df9fc5b14596608f2303fe19869f54
Aitor Ugarte <aitorugarte@opendeusto.es>
2018-04-15 16:19:45 +0200

    one more time deleted all

--- src/main/java/es/deusto/bspq18/e6/DeustoBox/Server/jdo/dao/IDeustoBoxDAO.java
+++
@@ -1,9 +0,0 @@
-package es.deusto.bspq18.e6.DeustoBox.Server.jdo.dao;
-
-import es.deusto.bspq18.e6.DeustoBox.Server.jdo.data.User;
-
-public interface IDeustoBoxDAO {
-	
-	public User getUser(String email, String pass);
-	public boolean addUser(User e);
-}
