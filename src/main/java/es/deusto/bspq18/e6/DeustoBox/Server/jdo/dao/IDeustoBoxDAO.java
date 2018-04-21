3f0117f6b6be134495e536fefaaf6d94a0a6632e
Markel <Markel@Markel-PC.home>
2018-04-21 15:42:17 +0200

    Errores

--- src/main/java/es/deusto/bspq18/e6/DeustoBox/Server/jdo/dao/IDeustoBoxDAO.java
+++
@@ -1,9 +0,0 @@
-package es.deusto.bspq18.e6.DeustoBox.Server.jdo.dao;
-
-import es.deusto.bspq18.e6.DeustoBox.Server.jdo.data.DUser;
-
-public interface IDeustoBoxDAO {
-	
-	public DUser getUser(String email, String pass);
-	public boolean addUser(DUser e);
-}
--- src/main/java/es/deusto/bspq18/e6/DeustoBox/Server/utils/Error_log.java
+++
@@ -1,5 +0,0 @@
-package es.deusto.bspq18.e6.DeustoBox.Server.utils;
-
-public class Error_log {
-
-}
