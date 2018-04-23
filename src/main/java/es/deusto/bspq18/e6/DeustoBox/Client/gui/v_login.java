c2070a6ccf801ba2a7367438edb527210ebb7ff0
Aitor Ugarte <aitorugarte@opendeusto.es>
2018-04-23 21:02:23 +0200

    login gui updated

--- src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/gui/v_login.java
+++ src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/gui/v_login.java
@@ -9,40 +9,42 @@ import javax.swing.border.EmptyBorder;
 
 import es.deusto.bspq18.e6.DeustoBox.Client.controller.Controller;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 
 public class v_login extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JPanel contentPane, panel;
 	private JTextField txtUsername;
 	private JPasswordField passwordField;
-	private JPanel panel;
-	private JLabel lblLogin;
-	private JLabel lblUsername;
-	private JLabel lblPassword;
-	private JButton btnEnter;
-	private JLabel lblRegister;
-	private JButton btnCreateAccount;
+	private JLabel lblLogin, lblUsername, lblPassword, lblRegister, lblLogo;
+	private JButton btnEnter, btnCreateAccount;
 	private Controller controlador;
-	private v_client window;
+
 
 	/**
 	 * Create the frame.
 	 */
+
 	public v_login(Controller controlador) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(v_login.class.getResource("/es/deusto/bspq18/e6/DeustoBox/Client/images/logo.png")));
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -51,37 +53,33 @@ public class v_login extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		this.controlador = controlador;
-		InitComponents();
-		this.setVisible(true);
-	}
-
-	public void InitComponents() {
+		
 		panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-
+		
 		lblLogin = new JLabel("Log In to DeustoBox");
 		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblLogin.setBounds(98, 16, 224, 35);
+		lblLogin.setBounds(168, 38, 224, 35);
 		panel.add(lblLogin);
-
+		
 		lblUsername = new JLabel("Username:");
-		lblUsername.setBounds(72, 74, 69, 20);
+		lblUsername.setBounds(47, 114, 83, 20);
 		panel.add(lblUsername);
-
+		
 		lblPassword = new JLabel("Password:");
-		lblPassword.setBounds(72, 110, 69, 20);
+		lblPassword.setBounds(47, 156, 83, 20);
 		panel.add(lblPassword);
-
+		
 		txtUsername = new JTextField();
-		txtUsername.setBounds(156, 71, 146, 26);
+		txtUsername.setBounds(145, 111, 146, 26);
 		panel.add(txtUsername);
 		txtUsername.setColumns(10);
-
+		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(156, 107, 146, 26);
+		passwordField.setBounds(145, 153, 146, 26);
 		panel.add(passwordField);
-
+		
 		btnEnter = new JButton("Sign In");
 		btnEnter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -108,13 +106,13 @@ public class v_login extends JFrame {
 				}
 			}
 		});
-		btnEnter.setBounds(169, 149, 115, 29);
+		btnEnter.setBounds(168, 183, 115, 29);
 		panel.add(btnEnter);
-
+		
 		lblRegister = new JLabel("Don't have an account yet?");
-		lblRegister.setBounds(87, 230, 186, 20);
+		lblRegister.setBounds(29, 214, 190, 20);
 		panel.add(lblRegister);
-
+		
 		btnCreateAccount = new JButton("Create an account");
 		btnCreateAccount.setForeground(Color.BLUE);
 		btnCreateAccount.addActionListener(new ActionListener() {
@@ -124,10 +122,28 @@ public class v_login extends JFrame {
 				dispose();
 			}
 		});
-		btnCreateAccount.setBounds(261, 226, 163, 29);
+		
+		btnCreateAccount.setBounds(220, 210, 163, 29);
 		btnCreateAccount.setBorderPainted(false);
 		btnCreateAccount.setContentAreaFilled(false);
 		panel.add(btnCreateAccount);
+	
+		lblLogo = new JLabel();	
+		lblLogo.setBounds(15, 14, 137, 81);
+		
+		URL url = this.getClass().getResource("/es/deusto/bspq18/e6/DeustoBox/Client/images/logo.png");
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Image dimg = img.getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(),
+		        Image.SCALE_SMOOTH);
+		
+		ImageIcon imageIcon = new ImageIcon(dimg);
+		lblLogo.setIcon(imageIcon);
+		
+		panel.add(lblLogo);
 	}
-
 }
