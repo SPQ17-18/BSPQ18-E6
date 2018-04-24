d4c89a0ed44729977f13b2e4195acc18f7e00e95
celine-fevrier <37117267+celine-fevrier@users.noreply.github.com>
2018-04-24 20:32:11 +0200

    Add files via upload

---
+++ Client/src/gui/v_client_profile_changepassword.java
@@ -0,0 +1,104 @@
+package es.deusto.bspq18.e6.DeustoBox.Client.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import es.deusto.bspq18.e6.DeustoBox.Client.controller.Controller;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+
+public class v_client_profile_changepassword extends JFrame {
+
+	private JPanel contentPane;
+	private Controller controlador;
+	private JPasswordField passwordField;
+	private JPasswordField passwordField_1;
+	private JLabel lblChangePassword;
+	private JLabel lblInsertThePassword;
+	private JLabel lblrepeatPassword;
+	private JButton btnConfirm;
+
+	/**
+	 * Create the frame.
+	 */
+	public v_client_profile_changepassword(Controller controlador) {
+		this.controlador = controlador;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 332, 260);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		InitComponents();
+		this.setVisible(true);
+		
+	}
+	public void InitComponents() {
+		lblChangePassword = new JLabel("Change Password");
+		lblChangePassword.setBounds(113, 6, 126, 16);
+		contentPane.add(lblChangePassword);
+		
+		lblInsertThePassword = new JLabel("Insert the password");
+		lblInsertThePassword.setBounds(107, 51, 132, 16);
+		contentPane.add(lblInsertThePassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(92, 79, 147, 26);
+		contentPane.add(passwordField);
+		
+		lblrepeatPassword = new JLabel("Repeat the password");
+		lblrepeatPassword.setBounds(107, 126, 132, 16);
+		contentPane.add(lblrepeatPassword);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(92, 154, 147, 26);
+		contentPane.add(passwordField_1);
+		
+		btnConfirm = new JButton("Confirm");
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(String.valueOf(passwordField.getPassword()).equals(String.valueOf(passwordField_1.getPassword()))) {
+					boolean correct = controlador.updatePassword(String.valueOf(passwordField.getPassword()));
+					if(correct){
+						JOptionPane.showMessageDialog(null, "Your password has been changed");
+						dispose();
+						
+					}
+					else{
+						JOptionPane.showMessageDialog(null, "An error has happened while changing the password, try it again");
+						
+					}
+					
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Passwords do not match");
+					
+				}
+			}
+		});
+		btnConfirm.setBounds(102, 192, 117, 29);
+		contentPane.add(btnConfirm);
+
+	
+
+	}
+
+		
+	
+	public Controller getControlador() {
+		return controlador;
+	}
+	public void setControlador(Controller controlador) {
+		this.controlador = controlador;
+	}
+}
---
+++ Client/src/gui/v_installer.java
@@ -0,0 +1,97 @@
+package es.deusto.bspq18.e6.DeustoBox.Client.gui;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import es.deusto.bspq18.e6.DeustoBox.Client.controller.Controller;
+
+import java.awt.Font;
+
+public class v_installer extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	protected JFrame frame;
+	private JTextField txtPath;
+	private JFileChooser fileChooser;
+	private Controller controlador;
+
+	/**
+	 * Create the application.
+	 */
+	public v_installer(Controller controlador) {
+		initialize();
+		this.controlador = controlador;
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Installer");
+		frame.setBounds(100, 100, 610, 309);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		txtPath = new JTextField();
+		txtPath.setBounds(31, 119, 398, 21);
+		frame.getContentPane().add(txtPath);
+		txtPath.setColumns(10);
+
+		JButton btnBrowse = new JButton("Browse");
+		btnBrowse.setBounds(465, 112, 87, 37);
+		frame.getContentPane().add(btnBrowse);
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btnCancel.setBounds(120, 180, 115, 29);
+		frame.getContentPane().add(btnCancel);
+
+		JButton btnOk = new JButton("Ok");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String dir = fileChooser.getSelectedFile().toString();
+				File directorio=new File(dir + "\\Client Deusto-Box");
+				controlador.setPath(dir + "\\Client Deusto-Box\\");
+				directorio.mkdir(); 
+				v_client client = new v_client(controlador);
+				client.setVisible(true);
+				dispose();
+			}
+		});
+		btnOk.setBounds(314, 180, 115, 29);
+		frame.getContentPane().add(btnOk);
+		
+		JLabel lblDeustoboxInstaller = new JLabel("DeustoBox installer");
+		lblDeustoboxInstaller.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblDeustoboxInstaller.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDeustoboxInstaller.setBounds(163, 26, 220, 48);
+		frame.getContentPane().add(lblDeustoboxInstaller);
+
+		btnBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileChooser = new JFileChooser();
+				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				fileChooser.setAcceptAllFileFilterUsed(false);
+
+				int rVal = fileChooser.showOpenDialog(null);
+				if (rVal == JFileChooser.APPROVE_OPTION) {
+					txtPath.setText(fileChooser.getSelectedFile().toString());
+				}
+			}
+		});
+	}
+}
