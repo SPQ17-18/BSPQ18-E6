95725622b97be696b3112aff20e9609fbb0c22cf
Aitor Ugarte <aitorugarte@opendeusto.es>
2018-05-20 17:06:11 +0200

    new client gui!!!

--- src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/gui/prueba.java
+++ src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/gui/prueba.java
@@ -10,9 +10,16 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.SwingConstants;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class prueba extends JFrame {
@@ -61,19 +68,65 @@ public class prueba extends JFrame {
 		contentPane.add(labelTitle);
 		
 		JButton button = new JButton("Folder");
-		button.setBounds(10, 111, 89, 23);
+		button.setBounds(27, 111, 89, 23);
 		contentPane.add(button);
 		
-		JButton btnSync = new JButton("Sync");
-		btnSync.setBounds(354, 111, 80, 23);
+		JButton btnSync = new JButton("");
+		btnSync.setBounds(392, 5, 47, 44);
+		URL url2 = this.getClass().getResource("/es/deusto/bspq18/e6/DeustoBox/Client/images/sync.png");
+		BufferedImage img2 = null;
+		try {
+			img2 = ImageIO.read(url2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Image dimg2 = img2.getScaledInstance(btnSync.getWidth()/2, btnSync.getHeight()/2,
+		        Image.SCALE_SMOOTH);
+		ImageIcon imageIcon2 = new ImageIcon(dimg2);
+		btnSync.setIcon(imageIcon2);
 		contentPane.add(btnSync);
 		
-		JButton btnSms = new JButton("Sms");
-		btnSms.setBounds(255, 111, 89, 23);
+		JButton btnSms = new JButton("");
+		btnSms.setBounds(392, 60, 47, 23);
+		URL url3 = this.getClass().getResource("/es/deusto/bspq18/e6/DeustoBox/Client/images/write.png");
+		BufferedImage img3 = null;
+		try {
+			img3 = ImageIO.read(url3);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Image dimg3 = img3.getScaledInstance(btnSync.getWidth()/2, btnSync.getHeight()/2,
+		        Image.SCALE_SMOOTH);
+		ImageIcon imageIcon3 = new ImageIcon(dimg3);
+		btnSms.setIcon(imageIcon3);
 		contentPane.add(btnSms);
 		
-		JButton btnPerfil = new JButton("Perfil");
-		btnPerfil.setBounds(156, 111, 89, 23);
+		JButton btnPerfil = new JButton("");
+		URL url = this.getClass().getResource("/es/deusto/bspq18/e6/DeustoBox/Client/images/perfil.png");
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Image dimg = img.getScaledInstance(btnSms.getWidth()/2, btnSms.getHeight()/2,
+		        Image.SCALE_SMOOTH);
+		
+		ImageIcon imageIcon = new ImageIcon(dimg);
+		btnPerfil.setIcon(imageIcon);
+		btnPerfil.setBounds(5, 5, 35, 30);
 		contentPane.add(btnPerfil);
+		
+		JLabel lblNmeroDeArchivos = new JLabel("Número de archivos: ");
+		lblNmeroDeArchivos.setBounds(27, 39, 125, 20);
+		contentPane.add(lblNmeroDeArchivos);
+		
+		JLabel lblMisMensajes = new JLabel("Mis mensajes:");
+		lblMisMensajes.setBounds(27, 59, 101, 20);
+		contentPane.add(lblMisMensajes);
+		
+		JButton btnLeerSms = new JButton("Leer sms");
+		btnLeerSms.setBounds(350, 94, 89, 23);
+		contentPane.add(btnLeerSms);
 	}
 }
