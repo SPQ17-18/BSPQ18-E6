3a350faa63f5e8253880eea80c1c89773e5b6bf1
Aitor Ugarte <aitorugarte@opendeusto.es>
2018-05-20 15:55:24 +0200

    Zzzz

--- src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/gui/prueba.java
+++ src/main/java/es/deusto/bspq18/e6/DeustoBox/Client/gui/prueba.java
@@ -8,6 +8,10 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 
@@ -44,14 +48,32 @@ public class prueba extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel label = new JLabel("Deusto Box Server");
-		label.setBounds(5, 5, 434, 20);
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setFont(new Font("Tahoma", Font.BOLD, 16));
-		contentPane.add(label);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
+        Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
+        int y = (int) rect.getMaxY() - getHeight() - getHeight()/3;
+        setLocation(0, y);
+        
+		JLabel labelTitle = new JLabel("Deusto Box Client");
+		labelTitle.setBounds(5, 5, 434, 20);
+		labelTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		labelTitle.setFont(new Font("Tahoma", Font.BOLD, 16));
+		contentPane.add(labelTitle);
 		
 		JButton button = new JButton("Folder");
-		button.setBounds(47, 111, 89, 23);
+		button.setBounds(10, 111, 89, 23);
 		contentPane.add(button);
+		
+		JButton btnSync = new JButton("Sync");
+		btnSync.setBounds(354, 111, 80, 23);
+		contentPane.add(btnSync);
+		
+		JButton btnSms = new JButton("Sms");
+		btnSms.setBounds(255, 111, 89, 23);
+		contentPane.add(btnSms);
+		
+		JButton btnPerfil = new JButton("Perfil");
+		btnPerfil.setBounds(156, 111, 89, 23);
+		contentPane.add(btnPerfil);
 	}
 }
